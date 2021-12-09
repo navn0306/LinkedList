3d2455eb71bfa139bae21cf7a4ce5e93b8715fad
Navneet <navn0306@gmail.com>
2021-12-09 15:23:54 +0530

    ADD: Welcome message for program

---
+++ src/com/bridge/linkedlists/Implementation.java
@@ -0,0 +1,7 @@
+package com.bridge.linkedlists;
+
+public class Implementation {
+    public static void main(String[] args) {
+        System.out.println("\n Welcome, this program represents the implementation of LinkedLists");
+    }
+}
